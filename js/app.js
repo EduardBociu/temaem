ec331d377ffd9741dc330df116724db8d48f96c6
EduardBociu <109726113+EduardBociu@users.noreply.github.com>
2022-07-22 12:48:54 +0300

    js

--- js/app.js
+++ js/app.js
@@ -1,37 +1,58 @@
-const tickets = [
-  { name: "Prize 1", ammount: 500 },
-  { name: "Prize 2", ammount: 250 },
-  { name: "Prize 3", ammount: 50 },
+const ticketsData = [
+  {
+    id: 1687512,
+    price: 2,
+  },
+  {
+    id: 238619,
+    price: 5,
+  },
+  {
+    id: 342620,
+    price: 20,
+  },
 ];
 
-class Log {
-  constructor(action, id) {
-    this.action = action;
-    this.button = id;
+let tickets = [];
+let ticketButtons = document.querySelectorAll("div.ticket");
+let buttons = document.querySelectorAll("button");
+
+class Ticket {
+  constructor(id, price) {
+    this.id = id;
+    this.price = price;
   }
 
-  alert() {
-    console.log(this);
+  addElement(element) {
+    this.element = element;
+  }
+
+  Log() {
+    console.log(`Value: ${this.price}\nId: ${this.id}`);
   }
 }
 
-let menu = document.querySelectorAll(".ticket");
-let buttons = document.querySelectorAll("button");
+class Button {
+  constructor(element) {
+    this.action = "click";
+    this.id = element.innerText;
+  }
 
-console.log("Da");
-let addEvents = () => {
-  for (let i = 0; i < menu.length; i++) {
-    menu[i].addEventListener("click", () => {
-      console.log(tickets[i]);
-    });
+  Log() {
+    console.log(`Action: ${this.action}, button: ${this.id}`);
+  }
+}
+
+const addEvents = () => {
+  for (let i = 0; i < ticketButtons.length; i++) {
+    let ticket = new Ticket(ticketsData[i].id, ticketsData[i].price);
+    ticket.addElement(ticketButtons[i]);
+    tickets.push(ticket);
+    ticketButtons[i].addEventListener("click", ticket.Log());
   }
 
   buttons.forEach((element) => {
-    element.addEventListener("click", () => {
-      let action = new Log("click", element.innerText);
-      action.alert();
-    });
+    let button = new Button(element);
+    element.addEventListener("click", button.Log());
   });
 };
-
-addEvents();
